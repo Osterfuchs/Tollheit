895caa8972e9cfac6e9195762b7878acedc549c3
Vogl01 <s.vogl@onoffic.de>
2024-11-13 12:10:16 +0100

    remove comments and clear lines

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-    print("HelloWorld!");
+    f = open("text.txt")
+    lines = list(filter(None, (line.split('#', 1)[0] for line in f.read().split('\n'))))
+    i = 0
+    for l in lines:
+        print(i, l)
+        i = i+1
 
 if __name__ == "__main__":
     main()
