ab7d70d4c73bc889ad7b4af6fdc07aafe62c5436
Vogl01 <s.vogl@onoffic.de>
2024-11-14 10:15:46 +0100

    remove blank lines

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+keywords = ["IF", "WHILE"]
+
 def main():
     f = open("text.txt")
-    lines = list(filter(None, (line.split('#', 1)[0] for line in f.read().split('\n'))))
+    # Get lines from File
+    lines = f.read().split('\n')
+    # remove comments
+    lines = [line.split('#', 1)[0] for line in lines]
+    # remove blanks
+    is_not_blank = lambda x: 
+        False if x == "" 
+        else True if x[0] not in [' ', '\n', '\t'] 
+        else is_not_blank(x[1:]);
+    lines = list(filter(is_not_blank, lines))
+
     i = 0
     for l in lines:
         print(i, l)
         i = i+1
+    
 
 if __name__ == "__main__":
     main()
